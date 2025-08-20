a5a635204b71dc258f8799ab967dc974cecdb48b
Satya <reptiledeveloper@gmail.com>
2025-08-20 21:17:14 +0530

    sdfdsfdsf

--- nutracore_admin/app/Imports/UserImport.php
+++ nutracore_admin/app/Imports/UserImport.php
@@ -2,6 +2,7 @@
 
 namespace App\Imports;
 
+use App\Helpers\CustomHelper;
 use App\Models\Products;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
@@ -16,7 +17,7 @@ class UserImport implements ToCollection, WithHeadingRow
         foreach ($rows as $row) {
 
             // --- 1. Update or Insert Product ---
-            User::updateOrCreate(
+            $user = User::updateOrCreate(
                 ['phone' => $row['phone'] ?? 0],
                 [
                     'name' => $row['name'] ?? '',
@@ -24,5 +25,21 @@ class UserImport implements ToCollection, WithHeadingRow
                 ]
             );
         }
+    }
+
+
+    public function creditNcCash($user, $amount)
+    {
+        $dbArray1 = [];
+        $dbArray1['userID'] = $user->id;
+        $dbArray1['txn_no'] = "NC" . rand(1111, 9999999);
+        $dbArray1['amount'] = $amount;
+        $dbArray1['wallet_type'] = "cashback_wallet";
+        $dbArray1['type'] = "CREDIT";
+        $dbArray1['note'] = "Earn NC Cash From Import ";
+        $dbArray1['against_for'] = 'cashback_wallet';
+        $dbArray1['paid_by'] = 'admin';
+        $dbArray1['orderID'] = 0;
+        CustomHelper::SaveTransaction($dbArray1);
     }
 }
