bf6ca86fc752831b03541e19a8f64aab0120c213
Satya <reptiledeveloper@gmail.com>
2025-08-20 20:50:31 +0530

    sdfdsfdsf

--- nutracore_admin/app/Http/Controllers/ProductController.php
+++ nutracore_admin/app/Http/Controllers/ProductController.php
@@ -409,7 +409,9 @@ class ProductController extends Controller
                     $index1 = $j - 1;
                     $excelArr['AttributeID' . $j] = $attributesArr[$index1]['attributes_id'] ?? '';
                     $excelArr['AttributeName' . $j] = $attributesArr[$index1]['attributes_name'] ?? '';
-                    $excelArr['Values' . $j] = $attributesArr[$index1]['attributes_values'] ?? '';
+                    $array = json_decode($attributesArr[$index1]['attributes_values'] ?? '', true);
+                    $string = '"' . implode('","', $array) . '"';
+                    $excelArr['Values' . $j] = $string ?? '';
                 }
 
 
